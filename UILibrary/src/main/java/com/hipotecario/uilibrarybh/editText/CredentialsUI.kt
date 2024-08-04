608c9f96fe0092d1c1a5fe49705f23da9103fecf
A05246 <a05246@bh.com.ar>
2024-08-04 16:19:00 -0300

    first commit

---
+++ UILibrary/src/main/java/com/hipotecario/uilibrarybh/editText/CredentialsUI.kt
@@ -0,0 +1,19 @@
+package com.hipotecario.uilibrarybh.editText
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import com.hipotecario.uilibrarybh.R
+
+class CredentialsUI @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+): LinearLayout(context, attrs, defStyleAttr, defStyleRes) {
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.credentials_ui, this, true)
+    }
+}
