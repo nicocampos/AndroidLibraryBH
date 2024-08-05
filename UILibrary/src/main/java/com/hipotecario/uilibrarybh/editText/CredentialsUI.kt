181c57be723eaf3cc52ddec476b90085d8c2fba1
A05246 <a05246@bh.com.ar>
2024-08-05 12:29:25 -0300

    update credential ui

--- UILibrary/src/main/java/com/hipotecario/uilibrarybh/editText/CredentialsUI.kt
+++ UILibrary/src/main/java/com/hipotecario/uilibrarybh/editText/CredentialsUI.kt
@@ -3,7 +3,11 @@ package com.hipotecario.uilibrarybh.editText
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.TextView
 import com.hipotecario.androidlibrary.R
 import com.hipotecario.androidlibrary.databinding.CredentialsUiBinding
 
@@ -15,14 +19,32 @@ class CredentialsUI @JvmOverloads constructor(
 ): LinearLayout(context, attrs, defStyleAttr, defStyleRes) {
     private var binding: CredentialsUiBinding
 
+    val tvDni: TextView
+    val editTextDni: EditText
+    val layoutUser: TextView
+    val rlUser: RelativeLayout
+    val editTextUser: EditText
+    val ivUserToggle: ImageView
+    val layoutPassword: TextView
+    val rlAAA: RelativeLayout
+    val editTextPassword: EditText
+    val ivBuhoPassToggle: ImageView
+
     init {
         val view = LayoutInflater.from(context)
             .inflate(R.layout.credentials_ui, this, true)
 
         binding = CredentialsUiBinding.bind(view)
-    }
 
-    fun setDNI(dni:String) {
-        binding.tvDni.text = dni
+        tvDni = binding.tvDni
+        editTextDni = binding.editTextDni
+        layoutUser = binding.layoutUser
+        rlUser = binding.rlUser
+        editTextUser = binding.editTextUser
+        ivUserToggle = binding.ivUserToggle
+        layoutPassword = binding.layoutPassword
+        rlAAA = binding.rlAAA
+        editTextPassword = binding.editTextPassword
+        ivBuhoPassToggle = binding.ivBuhoPassToggle
     }
 }
